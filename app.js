fb4b79be56a64c10949ea1625538a88162f83b1d
Richard Dotchin <richard.dotchin@gmail.com>
2017-03-09 01:22:09 -0500

    login working

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-var Nightmare = require('nightmare');       
-var nightmare = Nightmare({ show: true });
+const Nightmare = require('nightmare');       
+const nightmare = Nightmare({ show: true });
+const user = require('./login/login.js');
+const userName = user.username;         //put your username here
+const pass = user.password;             //put your password here
+
+
+
+
+
+
+logIn();
+
+function logIn(){
+	nightmare
+		.goto('https://www.facebook.com')
+		.type('#email', userName)
+		.type('#pass', pass)
+		.click('.uiButton')
+		.wait(5000)
+		.then(function(){
+		});
+}
+
+
+
+
